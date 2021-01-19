72c4cb92ce67efcdd72ecf0c2859d3a7fcb74963
Deadida322 <61591520+Deadida322@users.noreply.github.com>
2021-01-19 20:12:08 +0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.first_slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:false,
+        infinite: true,
+        slidesToShow:1,
+        dots: true,
+
+    });
+    $('.second_slider').slick({
+        arrows:true,
+        infinite: true,
+        slidesToShow:1,
+    });
+    $('.third_slider').slick({
+        arrows:false,
+        infinite: true,
+        slidesToShow:1,
+    });
+});
+  
